010d360c6389d963fbd26db9276a7ac72454fd43
emi <emiliorizzo@gmail.com>
2021-06-09 20:48:05 +0000

    Update File component, implement Spinner & FileChunks

--- dapps/images-client/src/views/File.vue
+++ dapps/images-client/src/views/File.vue
@@ -1,19 +1,30 @@
 <template lang="pug">
-.file
+.file(v-if="chainId && address")
+  h2 {{ address }}
+  h3 {{net.name}}
   template(v-if="cache.image")
     img.pic(:src="cache.image")
+  template(v-else)
+    spinner(v-if="!cache.size")
+    file-chunks(:chainId="chainId", :address="address")
   ul.meta(v-if="meta")
     li.title
-      span {{meta.title}}
+      span {{ meta.title }}
     li
-      span {{meta.description}}
-      small(v-if="meta.width && meta.height") &nbsp; ({{meta.width}} x {{meta.height}})
+      span {{ meta.description }}
+      small(v-if="meta.width && meta.height") &nbsp; ({{ meta.width }} x {{ meta.height }})
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import Spinner from '../components/Spinner.vue'
+import FileChunks from '../components/FileChunks.vue'
 export default {
   name: 'file',
   props: ['chainId', 'address'],
+  components: {
+    Spinner,
+    FileChunks
+  },
   data () {
     return {
       meta: {}
@@ -26,23 +37,33 @@ export default {
     getImage()
   },
   computed: {
-    ...mapGetters(['getCache']),
+    net () {
+      const { getNetwork, chainId } = this
+      return getNetwork()(chainId)
+    },
     cache () {
       const { getCache, address, chainId } = this
-      return getCache({ address, chainId })
+      return getCache()({ address, chainId })
     }
   },
   methods: {
+    ...mapGetters(['getCache', 'getNetwork']),
     ...mapActions(['createAddress'])
   }
 }
 </script>
 <style lang="stylus">
-  .file
-    min-height 100px
-  ul.meta
-    list-style none
-    text-align center
-    .title
-      font-weight bold
+.file
+  min-height 100px
+  display flex
+  flex-flow column wrap
+  align-items center
+  margin 1em
+
+ul.meta
+  list-style none
+  text-align center
+
+  .title
+    font-weight bold
 </style>
