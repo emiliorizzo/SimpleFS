3fbea4228d296c9d4317a474867d95a19c21e01c
emi <emiliorizzo@gmail.com>
2021-06-10 18:17:07 +0000

    Update Home component, implement AddressTitle

--- dapps/images-client/src/views/Home.vue
+++ dapps/images-client/src/views/Home.vue
@@ -1,23 +1,36 @@
 <template lang="pug">
 .home
-  h1 Angel Java Lopez
+  h1 Angel Java Lopez Paintings
   template(
     v-for="net, chainId in networks",
     v-if="Object.keys(net.images).length"
   )
     h2 {{ net.name }}
-    ul.images
+    ul.images.plain
       li(v-for="img, address in net.images")
-        router-link(:to="`/image/${chainId}/${address}`") {{ img.title }} - {{ img.description }}
+        router-link(:to="`/image/${chainId}/${address}`")
+          address-title(:meta="img" :address="address")
 </template>
 <script>
-// @ is an alias to /src
 import config from '../../src/lib/config'
+import AddressTitle from '@/components/AddressTitle.vue'
 
 export default {
   name: 'Home',
+  components: {
+    AddressTitle
+  },
   data () {
     return config
   }
 }
 </script>
+<style lang="stylus">
+  .home
+    display flex
+    flex-flow column wrap
+    align-items center
+  ul.images
+    > li
+      margin 1em
+</style>
