99d1ac0fc549df6f329d5c97f70245e37ddd2b8d
emi <emiliorizzo@gmail.com>
2021-06-10 23:55:52 +0000

    Add footer

--- dapps/images-client/src/App.vue
+++ dapps/images-client/src/App.vue
@@ -1,16 +1,35 @@
 <template lang="pug">
 #app
   nav#nav
-    .title
-      span ajlopez
+    router-link(to="/")
+      ul.title
+        li.brand ajlopez
+        li
+          small paintings
     ul.menu
       li
         router-link(to="/") Home
       li
         router-link(to="/about") About
-  router-view
+  section.content
+    router-view
+  footer#footer
+    ul.small
+      li June 2021
+    ul.menu
+      li
+        a(v-if="githubRepo", :href="githubRepo") gitHub
 </template>
-
+<script>
+export default {
+  name: 'app',
+  computed: {
+    githubRepo () {
+      return this.$store.state.config.githubUrl
+    }
+  }
+}
+</script>
 <style lang="stylus">
 @import './style/main.styl'
 </style>
