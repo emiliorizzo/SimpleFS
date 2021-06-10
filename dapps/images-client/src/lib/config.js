2231923900f8cc23bd418a7abc7c69d824f99de7
emi <emiliorizzo@gmail.com>
2021-06-10 15:52:05 +0000

    Update config, add explorers & networks

--- dapps/images-client/src/lib/config.js
+++ dapps/images-client/src/lib/config.js
@@ -1,19 +1,31 @@
 
-import rskTestnetImages from '../../../imagesexpress/lib/imagesdata'
-import ethImages from './ethImages'
-import { parseImages } from './utils'
+import images from './images'
 
 export default {
   networks: {
     1: {
       name: 'Ethereum Mainnet',
       node: '',
-      images: ethImages
+      images: images.ethMainnet,
+      explorer: 'https://etherscan.io'
+    },
+    3: {
+      name: 'Ethereum Ropsten',
+      node: '',
+      images: images.ethRopsten,
+      explorer: 'https://ropsten.etherscan.io'
+    },
+    30: {
+      name: 'RSK Mainnet',
+      node: 'https://public-node.rsk.co:443',
+      images: images.rskMainnet,
+      explorer: 'https://explorer.rsk.co'
     },
     31: {
       name: 'RSK Testnet',
       node: 'https://public-node.testnet.rsk.co:443',
-      images: parseImages(rskTestnetImages)
+      images: images.rskTestnet,
+      explorer: 'https://explorer.testnet.rsk.co'
     }
   }
 }
---
+++ dapps/images-client/src/lib/images.js
@@ -0,0 +1,11 @@
+import rskTestnetImages from '../../../imagesexpress/lib/imagesdata'
+import { parseImages } from './utils'
+
+const rskTestnet = parseImages(rskTestnetImages)
+
+export default {
+  ethMainnet: [],
+  ethRopsten: [],
+  rskMainnet: [],
+  rskTestnet
+}
